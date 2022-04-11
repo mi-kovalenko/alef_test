4c1eefa627b2be8beb08d4a20860c72f3e95b357
Maria Kovalenko <mi.kovalenko@mail.ru>
2022-04-11 15:27:38 +0200

    upload

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { reactive } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+const item = reactive({ count: 1 })
+function increment() {
+  item.count++;
+  }
+function decrement() {
+  if (item.count > 0) {
+    item.count--;
+  }
+  }
+
+</script>
+
+
+[ {name: some_shitty_product, maxCount: 20, poster: big_pictureUrul, thumbnail: small_picture_url}, {….}}
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="-" @click="decrement()"/>
+      <span>{{ item.count }}</span>
+      <HelloWorld msg="+" @click="increment()"/>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <button class="button"> {{ msg }} </button>
+</template>
+
+<style scoped>
+  .button {
+    border: 3px solid black;
+    background-color: aliceblue;
+  }
+</style>
+
